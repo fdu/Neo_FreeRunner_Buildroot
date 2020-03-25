5bc760a71bffaa5689c397ff7859c476946b7b0d
fdu <1050329+fdu@users.noreply.github.com>
2020-03-25 21:46:23 +0100

    check kernel configuration in wifi pre-up script

--- overlay/etc/network/pre-up-ar6000.sh
+++ overlay/etc/network/pre-up-ar6000.sh
@@ -2,6 +2,21 @@
 
 LOG=/var/log/messages
 INTERFACE=eth0
+KERNEL_CONFIG=/proc/config.gz
+
+echo Checking kernel configuration
+if [ ! -e $KERNEL_CONFIG ]
+then
+  echo "  $KERNEL_CONFIG is not present, are you using the correct kernel?"
+  exit 1
+fi
+echo "  $KERNEL_CONFIG is present"
+if ! zcat $KERNEL_CONFIG | grep -q "CONFIG_CFG80211=y"
+then
+  echo "  CFG80211 not compiled in kernel"
+  exit 1
+fi
+echo "  CFG80211 compiled in kernel"
 
 echo Powering down wifi
 om wifi power 0
@@ -14,11 +29,22 @@ echo Powering up wifi
 om wifi power 1
 while ! tail $LOG | grep -q "AR6000 Reg Code = 0x40000060"
 do
+  if dmesg | tail -n 5 | grep -q "ar6000_activate: Failed to activate"
+  then
+    echo "  failed to activate ar6000, will reboot in 5 seconds"
+    sleep 5
+    reboot
+  fi
   echo "  waiting"
   sleep 1
 done
 echo Setting up $INTERFACE
 ifconfig $INTERFACE up
+if [ $? -ne 0 ]
+then
+  echo "  ifconfig failed"
+  exit 1
+fi
 while ! ifconfig $INTERFACE | grep -q $INTERFACE
 do
   echo "  waiting"
